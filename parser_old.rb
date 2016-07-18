ea01f3191b12274ab59ff1029eb759a44e00faad
Oleg Gushul <gushul.o@gmail.com>
2016-07-18 18:23:26 +0300

    WIP: refactoring

--- parser_old.rb
+++ parser_old.rb
@@ -2,20 +2,20 @@ require 'rubyXL'
 require 'json'
 
 
-dubug_file = open("output.txt", 'w')
+output_file = open("output.txt", 'w')
 result_file = open("result.txt", 'w')
-dubug_file.write("Begin\n")
+output_file.write("Begin\n")
 workbook = RubyXL::Parser.parse("rating_2015.xlsm")
 worksheet = workbook.worksheets[0]
 total = 0
 worksheet.each { |row| total +=1}
-dubug_file.write("total is #{total}\n" )
+output_file.write("total is #{total}\n" )
 index = 1
 cell = worksheet.sheet_data[index][0]
 
 
 
-dubug_file.write("Before while\n")
+output_file.write("Before while\n")
 #while !cell
 while index < total
   #sheet_data = worksheet.sheet_data[index]
@@ -26,48 +26,48 @@ while index < total
   if current == "Институт / факультет"
     cell = worksheet.sheet_data[index][5] if worksheet.sheet_data[index]
     current_inst  = cell && cell.value
-    dubug_file.write("Institute: #{current_inst}\n")
+    output_file.write("Institute: #{current_inst}\n")
     results.store(current_inst, "")
     spec_hash = Hash.new
 
     index +=1
     cell = worksheet.sheet_data[index][5] if worksheet.sheet_data[index]
     current_spec  = cell && cell.value
-    dubug_file.write("speciality: #{current_spec}\n")
+    output_file.write("speciality: #{current_spec}\n")
     spec_hash.store(current_spec, "")
     spec_set = Hash.new
 
     index +=2
     cell = worksheet.sheet_data[index][1] if worksheet.sheet_data[index]
     spec_set_name  = cell && cell.value
-    dubug_file.write("set: #{spec_set_name}\n")
+    output_file.write("set: #{spec_set_name}\n")
 
     index +=1
     cell = worksheet.sheet_data[index][2] if worksheet.sheet_data[index]
     plan  = cell && cell.value
-    dubug_file.write("plan: #{plan}\n")
+    output_file.write("plan: #{plan}\n")
 
     index +=1
     cell = worksheet.sheet_data[index][2] if worksheet.sheet_data[index]
     submitted  = cell && cell.value
-    dubug_file.write("submitted: #{submitted}\n")
+    output_file.write("submitted: #{submitted}\n")
 
     index +=1
     cell = worksheet.sheet_data[index][2] if worksheet.sheet_data[index]
     contest = cell && cell.value
-    dubug_file.write("contest: #{contest}\n")
+    output_file.write("contest: #{contest}\n")
 
     index +=5
     cell = worksheet.sheet_data[index][1] if worksheet.sheet_data[index]
     rating = cell && cell.value
 
-    dubug_file.write("#{rating}:\n")
+    output_file.write("#{rating}:\n")
 	  if rating == "Рейтинг"
 	    index +=2
 	    cell = worksheet.sheet_data[index][2]
 	    title  = cell && cell.value
 
-	    dubug_file.write("#{title}:\n")
+	    output_file.write("#{title}:\n")
 
 	    while
 	      index += 1
@@ -81,7 +81,7 @@ while index < total
 		      cell = worksheet.sheet_data[index][5]
 		      sum  = cell && cell.value
 		      result_file.write("student: #{rate}, #{id}, #{name}, #{sum} \n")
-		      dubug_file.write("student: #{rate}, #{id}, #{name}, #{sum} \n")
+		      output_file.write("student: #{rate}, #{id}, #{name}, #{sum} \n")
 	      else
           break
 	      end
@@ -91,24 +91,5 @@ while index < total
 
   index += 1
 end
-dubug_file.close
 
-def get_rating index, sheet,dubug_file
-  while
-    if worksheet.sheet_data[index]
-      cell = worksheet.sheet_data[index][2]
-      rate  = cell && cell.value
-      cell = worksheet.sheet_data[index][3]
-      id  = cell && cell.value
-      cell = worksheet.sheet_data[index][4]
-      name  = cell && cell.value
-      cell = worksheet.sheet_data[index][5]
-      sum  = cell && cell.value
-      result_file.write("student: #{rate}, #{id}, #{name}, #{sum} \n")
-      dubug_file.write("student: #{rate}, #{id}, #{name}, #{sum} \n")
-    else
-      break
-    end
-  end
-end
-
+output_file.close
